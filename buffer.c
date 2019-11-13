4a3e620ffd9a9cdb5b112fd7fee56aa95c0ec502
imlgit-debug <57432772+imlgit-debug@users.noreply.github.com>
2019-11-13 10:20:19 +0900

    Add files via upload

--- buffer.c
+++ buffer.c
@@ -19,6 +19,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <linux/kernel.h>
 
 #include "iio-config.h"
 #include "iio-private.h"
@@ -134,7 +135,8 @@ ssize_t iio_buffer_refill(struct iio_buffer *buffer)
 	ssize_t read;
 	const struct iio_device *dev = buffer->dev;
 
-	printf("IML Debug -- iio_buffer_refill()\r\n");
+	printf("printf: IML Debug -- iio_buffer_refill()\r\n");
+	printk("printk: IML Debug -- iio_buffer_refill()\r\n");
 
 	if (buffer->dev_is_high_speed) {
 		read = dev->ctx->ops->get_buffer(dev, &buffer->buffer,
